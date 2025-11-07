c194b6d488abda659c594e49285f9133020e7fd1
5F\ktc <>
2025-11-07 19:46:13 +0900

    タイトル画面のストーリー(?)文章表示画面の作成途中（文章表示まで）

--- Assets/Scripts/TitleScene/Title_StartButton.cs
+++ Assets/Scripts/TitleScene/Title_StartButton.cs
@@ -6,20 +6,27 @@ using System.Collections.Generic;
 public class Title_StartButton : MonoBehaviour
 {
     public List<Text> text = new List<Text>();
-    public float delay;
+    public float delay { get; private set; } //文章の表示スピード
+    public GameObject Textobj;   //
+    public GameObject FadeImage; //フェードイメージを入れる
     private string Text;
+    private bool IsAccela = false;
 
 
     private void Start()
     {
+        delay = 0.05f;
         Text = text[0].text;
         text[0].text = "";
     }
-    //メインシーンへの遷移
+    //ストーリー（？）文章画面表示
     public void OnTitileButton()
     {
         Debug.Log("Start");
         StartCoroutine(ShowText());
+        IsAccela = true;
+        Textobj.SetActive(true);
+        FadeImage.SetActive(true);
     }
 
     IEnumerator ShowText()
@@ -28,6 +35,18 @@ public class Title_StartButton : MonoBehaviour
         {
             text[0].text += c;
             yield return new WaitForSeconds(delay);
+        }
+    }
+
+    private void Update()
+    {
+        if(IsAccela)
+        {
+            if(Input.GetMouseButton(0))
+            {
+                delay = 0f;
+                IsAccela = false;
+            }
         }
     }
 }
