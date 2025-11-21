60282ed24b138e99d5b69e871d05bff61ec8d3f1
5F\ktc <>
2025-11-21 18:26:03 +0900

    マップ選択画面、クリア画面仮完成

---
+++ Assets/Scripts/MapScene/MapSelectButton.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MapSelectButton : MonoBehaviour
+{
+    public void SelectGrassland()
+    {
+        SceneManager.LoadScene("");
+    }
+
+    public void SelectVolcano()
+    {
+        SceneManager.LoadScene("");
+    }
+}
