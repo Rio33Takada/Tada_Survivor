1548c9adabc763267dcbd0db65f85dde62c07fd5
5F\ktc <>
2025-12-12 19:11:42 +0900

    草原、火山マップの仮作成＆ゲームオーバー時のリトライボタン作成途中

--- Assets/Scripts/GameoverUI/GameoverButton.cs
+++ Assets/Scripts/GameoverUI/GameoverButton.cs
@@ -3,10 +3,18 @@ using UnityEngine.SceneManagement;
 
 public class GameoverButton : MonoBehaviour
 {
+    //ÉVÅ[ÉìÇÃñºëOäiî[
+    public string[] sceneName =
+    {
+        "Volcano_Map",
+        "Glassrand_Map"
+    };
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public void Retry()
     {
-        SceneManager.LoadScene("");
+        SceneManager.LoadScene(sceneName[0]);
     }
 
     public void Title()
