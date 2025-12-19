83ee30559f0cb6bb7020fb24569f7e29d4c67a16
5F\ktc <>
2025-12-19 17:42:41 +0900

    ゲームオーバー画面のリトライ修理

--- Assets/Scripts/GameoverUI/GameoverButton.cs
+++ Assets/Scripts/GameoverUI/GameoverButton.cs
@@ -3,18 +3,13 @@ using UnityEngine.SceneManagement;
 
 public class GameoverButton : MonoBehaviour
 {
-    //ÉVÅ[ÉìÇÃñºëOäiî[
-    public string[] sceneName =
-    {
-        "Volcano_Map",
-        "Glassrand_Map"
-    };
-
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public void Retry()
     {
-        SceneManager.LoadScene(sceneName[0]);
+        Scene Nscene = SceneManager.GetActiveScene();
+        string sceneName = Nscene.name;
+        SceneManager.LoadScene(sceneName);
     }
 
     public void Title()
