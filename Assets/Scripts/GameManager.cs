126ca2117f939651ee7c2c4933200978c018c1f9
5F\ktc <>
2025-12-19 14:42:18 +0900

    pointUI作成

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -58,6 +58,7 @@ public class GameManager : MonoBehaviour
 
     private void InitializeStats()
     {
+        RestorePoints();
         AttackPoint = initialAttackPoint;
         MovePoint = initialMovePoint;
         WaveCount = 0;
