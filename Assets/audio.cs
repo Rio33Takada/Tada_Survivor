789e0cc640f0ef29ac81460b01348541fa893671
a <KTC25A31A0002@edu.kyoto-tech.ac.jp>
2026-02-06 14:53:05 +0900

    選択時の音

---
+++ Assets/audio.cs
@@ -0,0 +1,18 @@
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+
+public class audio : MonoBehaviour
+{
+    public int AudioNum;
+    public List<AudioClip> AudioClipList;
+
+    public void OnButton()
+    {
+        if (AudioNum == 0)
+        {
+
+        }
+
+    }
+}
